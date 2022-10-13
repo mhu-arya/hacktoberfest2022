741071fd9ffe5ab8367e55d39a437926d2dfd2af
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-10-13 13:17:51 +0530

    added the main function || print menu function

--- abhay-ag/project_1/atm_project.cpp
+++ abhay-ag/project_1/atm_project.cpp
@@ -31,3 +31,50 @@ void addData(string name, string pass);
 void deposit(int index, string uname);
 void withdraw(int index, string uname);
 void request(int index, string uname);
+
+/*
+    The main function just invokes the start function which kicks off the rest of the program;
+
+    Start Function:
+        1. Calls the printIntroMenu() Function to print the entry menu;
+        2. User selects the options shown and the program moves forward accordingly.
+*/
+
+int main(){
+    cout << "Hello, User!! Welcome to this ATM Project!" << endl;
+    cout << endl;
+    start();
+    return 0;
+}
+
+void start(){
+    cout << "Please Select an option from the menu below: " << endl;
+    printIntroMenu();
+}
+
+/*
+    printIntroMenu() function:
+        1. Asks the user for the choice of the function he/she wants to perform
+        2. Proceeds the program accordingly.
+        3. If the user enters wrong choice he/she will be prompted to enter the values again.
+*/
+
+void printIntroMenu(){
+    cout << "l -> Login\nc -> Create New Account\nq -> Quit\n\n> " ;
+    cin >> menuInput;
+    switch (menuInput){
+        case 'l':
+            login();
+            break;
+        case 'c':
+            createAccount();
+            break;
+        case 'q':
+            exit(0);
+            break;
+        default:
+            cout << "Please enter correct input!!! \n" << endl;
+            printIntroMenu();
+            break;
+    }
+}
